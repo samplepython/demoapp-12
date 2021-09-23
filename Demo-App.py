aeef3abe7bf08c77eba3bd46cf647fc28ad52c65
raghu <raghu.mail12@yahoo.com>
2021-09-23 12:48:22 -0400

    rearranged the files

--- Demo-App.py
+++ Demo-App.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from BasicFunctions import read_markdown_file
 from pathlib import Path
 import sys
 
 def begin():
     st.title('Welcome to 5G Portable Machines')
-    company_description_file = str(Path.cwd())+'/StaticFiles/Company_Description.md'
+    company_description_file = str(Path.cwd())+'/static/Company_Description.md'
     markdown_company_description_text = read_markdown_file(company_description_file)
     st.markdown(markdown_company_description_text, unsafe_allow_html=False)
 
@@ -14,7 +13,7 @@ def machines():
 
 
 def contact_us():
-    contact_us_file = str(Path.cwd())+'/StaticFiles/Contact_Us.md'
+    contact_us_file = str(Path.cwd())+'/static/Contact_Us.md'
     contact_us_checkbox = st.sidebar.checkbox('Contact Us', True)
     if contact_us_checkbox:
         markdown_contact_us_text = read_markdown_file(contact_us_file)
@@ -22,8 +21,11 @@ def contact_us():
 
 if __name__ == '__main__':
     current_working_dir = str(Path.cwd())
-    seed_sowing_path = current_working_dir+'/Seed_Sowing/'
+    basic_src_path = current_working_dir+'/src'
+    sys.path.insert(0, basic_src_path)
+    seed_sowing_path = current_working_dir+'/seed_sowing'
     sys.path.insert(0, seed_sowing_path)
+    from BasicFunctions import read_markdown_file
     from SeedSowing import seed_sowing
 
     begin()
---
+++ seed_sowing/src/SeedSowing.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from pathlib import Path
+
+main_working_dir = str(Path.cwd())
+basic_src_path = main_working_dir+'/src'
+sys.path.insert(0, basic_src_path)
+from BasicFunctions import load_image_file, read_video_file, read_markdown_file
+
+def seed_sowing():
+    seed_sowing_machine_checkbox = st.sidebar.checkbox('Seed Sowing Machine', False)
+    if seed_sowing_machine_checkbox:
+        st.header('Seed Sowing Machine')
+        # Description
+        seed_sowing_description_file = main_working_dir+'/seed_sowing/static/Seed_Sowing_Description.md'
+        markdown_seed_sowing_description_text = read_markdown_file(seed_sowing_description_file)
+        st.markdown(markdown_seed_sowing_description_text, unsafe_allow_html=False)
+
+
+        # Images
+        st.caption('Basic Model')
+        image_file = main_working_dir + '/seed_sowing/media/Machine.jpg'
+        loaded_image = load_image_file(image_file)
+        st.image(loaded_image)
+
+	# Videos
+	st.header('Onion Seeds Sowing Demo')
+	onion_video_file = main_working_dir+'/seed_sowing/media/OnionSowing.mp4'
+	video_bytes = read_video_file(onion_video_file)
+	st.video(video_bytes)
